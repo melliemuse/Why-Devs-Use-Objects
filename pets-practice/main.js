dd24c0e19f95b30fcd00a31957b983309360251f
Melody Stern <melliemuse@gmail.com>
2019-10-11 18:12:00 -0500

    updated javascript for current exercise

--- pets-practice/main.js
+++ pets-practice/main.js
@@ -17,3 +17,24 @@ const pet = {
 pet.cuddle();
 pet.eat('ramen');
 pet.nap('laptop');
+
+// Give your pet a new key named favoriteToys whose value is an empty array.
+
+pet.favoriteToys = [];
+console.log(pet);
+
+// Now define another new key named play whose value is a function with a single parameter named toy.
+
+// The function should determine if your pet liked the toy, and if s/he does, then it should be added to the favoriteToys array using the this keyword (see example above).
+
+// Here's an example. If your pet is a cat, then he likely only likes furry toys, so you could put an if..then condition in the play() method that checks if the word "fuzzy" is in the argument value. If it is, it gets added to the array.
+
+pet.play = function(toy) {
+  if (toy.includes('string')) {
+    this.favoriteToys.push(toy);
+    return pet.favoriteToys;
+  }
+}
+
+console.log(pet.play('string cheese'));
+
